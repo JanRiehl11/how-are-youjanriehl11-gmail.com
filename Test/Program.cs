72359a0d65954697b34a90b22849909a6faaef09
Jan <Jan@DESKTOP-IP25U3O>
2023-10-13 11:04:34 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("hoe oud ben jij");
+var verjaardag.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}!");
+Console.WriteLine($"{Environment.NewLine}Hello, {name}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
